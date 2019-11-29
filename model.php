62a440707769889575717afc8ab3bda8f761270e
Nathan <nathan.barbet@hotmail.fr>
2019-11-29 12:02:46 +0100

    MVC Structure

---
+++ model.php
@@ -0,0 +1,30 @@
+<?php
+function dbConnect()
+{
+    try
+    {
+        $db = new PDO('mysql:host=localhost;dbname=oc_projet_5;charset=utf8', 'root', 'root');
+        return $db;
+    }
+    catch(Exception $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    }
+}
+function getPosts()
+{
+    $db = dbConnect();
+    $req = $db->query('SELECT ID, Title, Lead, Content, Date_publish FROM posts ORDER BY Date_publish DESC LIMIT 0, 5');
+
+    return $req;
+}
+
+function getPost($postId)
+{
+    $db = dbConnect();
+    $req = $db->prepare('SELECT ID, Title, Lead, Content, Date_publish FROM posts WHERE id = ?');
+    $req->execute(array($postId));
+    $post = $req->fetch();
+
+    return $post;
+}
