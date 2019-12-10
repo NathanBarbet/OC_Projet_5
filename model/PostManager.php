33640b799231e293f74ff27bef332c39490f70bb
NathanBarbet <nathan.barbet@hotmail.fr>
2019-12-10 09:17:24 +0100

    .

--- model/PostManager.php
+++ model/PostManager.php
@@ -3,21 +3,20 @@ require_once("model/DbManager.php");
 
 class PostManager extends DbManager
 {
-
     public function getPosts()
     {
         $db = $this->dbConnect();
-        $req = $db->query('SELECT ID, Title, Post_lead, Content, Img, DATE_FORMAT(Date_publish, \'%d/%m/%Y à %Hh%imin%ss\') AS Date_publish_fr FROM posts ORDER BY Date_publish DESC LIMIT 0, 5');
+        $posts = $db->query('SELECT ID, Title, Post_lead, Content, Img, DATE_FORMAT(Date_publish, \'%d/%m/%Y à %Hh%imin%ss\') AS Date_publish_fr FROM posts ORDER BY Date_publish DESC LIMIT 0, 5');
 
-        return $req;
+        return $posts;
     }
 
     public function getPost($postId)
     {
         $db = $this->dbConnect();
-        $req = $db->prepare('SELECT ID, Title, Post_lead, Content, Img, DATE_FORMAT(Date_publish, \'%d/%m/%Y à %Hh%imin%ss\') AS Date_publish_fr FROM posts WHERE ID = ?');
-        $req->execute(array($postId));
-        $post = $req->fetch();
+        $posts = $db->prepare('SELECT ID, Title, Post_lead, Content, Img, DATE_FORMAT(Date_publish, \'%d/%m/%Y à %Hh%imin%ss\') AS Date_publish_fr FROM posts WHERE ID = ?');
+        $posts->execute(array($postId));
+        $post = $posts->fetch();
 
         return $post;
     }
